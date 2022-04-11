94d844309ab8cfd805a3c5a4ab1e3ecd54235f1a
rnce414 <99392812+rnce414@users.noreply.github.com>
2022-04-11 17:49:11 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './assets/scss/main.css';
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
